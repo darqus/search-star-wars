87fc7b20e1e5018292976b248c94de4bb398eafb
eldar.ovchinnikov <eldar.ovchinnikov@lenta.com>
2022-03-24 01:09:47 +0300

    init start app

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col></v-col>
+      <v-col>
+        <h1 class="display-1 font-weight-bold mb-3">Welcome to Vuetify</h1>
+
+        <v-text-field v-model="search" label="Users" clearable></v-text-field>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Autocomplete',
+
+  data: () => ({
+    search: 'Hey!',
+  }),
+}
+</script>
