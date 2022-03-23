6318d787101d407d78f2845cd9e1c3aef37ca58f
eldar.ovchinnikov <eldar.ovchinnikov@lenta.com>
2022-03-24 01:34:31 +0300

    add ThemeSwitcher

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,21 +1,25 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col></v-col>
+      <v-col
+        ><h1 class="display-1 font-weight-bold mb-3">Welcome to Vuetify</h1>
+      </v-col>
       <v-col>
-        <h1 class="display-1 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
         <v-text-field v-model="search" label="Users" clearable></v-text-field>
       </v-col>
-      <v-col></v-col>
+      <v-col><ThemeSwitcher /></v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import ThemeSwitcher from '@/components/ThemeSwitcher.vue'
+
 export default {
   name: 'Autocomplete',
-
+  components: {
+    ThemeSwitcher,
+  },
   data: () => ({
     search: 'Hey!',
   }),
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card :dark="$vuetify.theme.dark" flat>
+    <v-toolbar flat height="72">
+      <v-switch
+        v-model="$vuetify.theme.dark"
+        hint="This toggles the global state of the Vuetify theme"
+        inset
+        :label="label"
+        persistent-hint
+      ></v-switch>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ThemeSwitcher',
+  computed: {
+    label() {
+      const isDark = this.$vuetify.theme.dark
+      const theme = isDark ? 'Dark' : 'Light'
+      const label = `Vuetify Theme ${theme}`
+      return label
+    },
+  },
+}
+</script>
