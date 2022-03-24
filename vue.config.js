f331ef07d92e863ba7c0fecb1f52f0436702c2c2
eldar.ovchinnikov <eldar.ovchinnikov@lenta.com>
2022-03-24 17:29:34 +0300

    fix indents

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-	publicPath: '',
-	outputDir: 'dist',
-	devServer: {
-		disableHostCheck: true,
-	},
-	transpileDependencies: ['vuetify'],
+  publicPath: '',
+  outputDir: 'dist',
+  devServer: {
+    disableHostCheck: true,
+  },
+  transpileDependencies: ['vuetify'],
 }
