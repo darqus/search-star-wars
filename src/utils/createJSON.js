cd98f4a937f12feb446ff7570ebc04459e3269d9
vol_dev <voladores@inbox.ru>
2022-04-05 01:28:11 +0300

    add renderJSON

--- src/utils/createJSON.js
+++ src/utils/createJSON.js
@@ -1,8 +1,10 @@
+const renderJSON = (data, params = [null, 2]) => JSON.stringify(data, ...params)
+
 export const createJSON = ({ ...data }) => {
 
   const filteredBooleanJSON = Object
     .entries(data)
     .reduce((acc, [key, value]) => (value ? { ...acc, [key]: value } : acc), {})
 
-  return JSON.stringify(filteredBooleanJSON, null, 2)
+  return renderJSON(filteredBooleanJSON)
 }
