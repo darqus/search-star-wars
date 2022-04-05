555e2412e8d7cdd2645e558b498aeead54d0ccbe
vol_dev <voladores@inbox.ru>
2022-04-05 15:56:52 +0300

    add Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="link" target="_blank" v-text="text" />
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    link: {
+      type: String,
+      default: '#',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
