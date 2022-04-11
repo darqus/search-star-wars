a7abe1539ba74c7f466d401db29a6722b2d03e6b
darqus <voladores@inbox.ru>
2022-04-11 06:43:25 +0300

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 VERSION=`grep -o '"version": "[^"]*' package.json | grep -o '[^"]*$'`
-
 CDATE=`date +"%Y-%m-%d %T"`
+COMMENT='v: '$VERSION' from '$CDATE
 
-COMMENT='dateTime: '$CDATE', v: '$VERSION
-echo 'commit name: '$COMMENT
+echo
+echo 'git commit name:'
+echo '"'$COMMENT'"'
+echo
 
 vue-cli-service build
 
 git checkout gh-pages
 
 rm -r dist/
-
 cp -r ../ssw_dist ./dist
 
 git add .
-
 git commit -m "$COMMENT"
-
 git push
-
 git checkout main
