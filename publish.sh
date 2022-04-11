6f084fc8b6e0d1d12bd90bbab17c9f939e35f7dc
darqus <voladores@inbox.ru>
2022-04-11 03:33:14 +0300

    add publish gh-pages

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git checkout gh-pages
+
+vue-cli-service build
+
+git checkout gh-pages
+
+git add .
+
+data=$Y$M$D$H$M$S
+echo $data
+echo 'upload'
+
+git commit -m "upload $data"
+
+git push
+
+git checkout main
