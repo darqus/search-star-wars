75a3e14202dcd53b21498695d666e3b243698521
darqus <voladores@inbox.ru>
2022-04-16 22:33:10 +0300

    add VUE_APP_VERSION obn build

--- publish.sh
+++ publish.sh
@@ -2,7 +2,14 @@
 
 VERSION=`grep -o '"version": "[^"]*' package.json | grep -o '[^"]*$'`
 CDATE=`date +"%Y-%m-%d %T"`
+
 sed -i "s/^VUE_APP_BUILD_DATA.*/VUE_APP_BUILD_DATA=$CDATE/g" .env.local
+
+prop="version"
+VERSION="$(node -e "console.log(require('./package.json')['$prop'])")"
+echo "$prop: '$VERSION'"
+sed -i "s/^VUE_APP_VERSION.*/VUE_APP_VERSION=$VERSION/g" .env.local
+
 COMMENT='v: '$VERSION' from '$CDATE
 
 echo
--- set-current-date.sh
+++ set-current-date.sh
@@ -4,3 +4,9 @@ CDATE=`date +"%Y-%m-%d %T"`
 echo $CDATE
 
 sed -i "s/^VUE_APP_BUILD_DATA.*/VUE_APP_BUILD_DATA=$CDATE/g" .env.local
+
+prop="version"
+VERSION="$(node -e "console.log(require('./package.json')['$prop'])")"
+echo "$prop: '$VERSION'"
+
+sed -i "s/^VUE_APP_VERSION.*/VUE_APP_VERSION=$VERSION/g" .env.local
