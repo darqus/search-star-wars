0e718a2903c4654985f2c709931bef32220fc54f
darqus <voladores@inbox.ru>
2025-06-26 23:46:05 +0300

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,10 @@ module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
   // transpileDependencies: true,
   lintOnSave: true,
+  configureWebpack: {
+    performance: {
+      // Disable asset size limit warnings
+      hints: false,
+    },
+  },
 })
